e41b619e1cd207a479a218d628a829c524190e75
Chandler S A Pan <snakepoon@gmail.com>
2022-02-08 23:25:21 +0800

    parse command options param

--- imooc-cli/bin/index.js
+++ imooc-cli/bin/index.js
@@ -1,4 +1,16 @@
 #!/usr/bin/env node
 const lib = require('imooc-cli-lib');
-console.log(lib.sum(1,2));
-console.log('imooc cli !!!');
+const argv = require('process').argv;
+const command = argv[2];
+const options = argv.slice(3);
+const [option,param] = options;
+console.log(option);
+console.log(param);
+// console.log(argv)
+if(lib[command]){
+    lib[command](option,param);
+}else{
+    console.log('请输入命令');
+}
+// console.log(lib.sum(1,2));
+// console.log('imooc cli !!!');
